815c8376effd84be8c637bc08d74772514b8d399
arsalansufi <arsalansufi@gmail.com>
2016-04-29 11:02:38 -0400

    Rename TutoringActivity interface to TCPClientOwner for clarity

--- src/tutoringproject/breaks/TCPClientOwner.java
+++ src/tutoringproject/breaks/TCPClientOwner.java
@@ -5,7 +5,7 @@ import android.os.Looper;
 /**
  * Created by arsalan on 4/21/16.
  */
-public interface TutoringActivity {
+public interface TCPClientOwner {
     Looper getMainLooper();
     void disableButtons();
     void messageReceived(String message);
