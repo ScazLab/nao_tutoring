0d1a806f6f7fe3a1b924b81c11f4d93ebdd779a4
Kevin Jiang <k.jiang@yale.edu>
2016-05-04 02:58:35 -0400

    implemented super rules 2 and 3

--- python_scripts/breaks.py
+++ python_scripts/breaks.py
@@ -23,14 +23,16 @@ def map_break_message(b):
     return dic[b]
 
 
-def take_break(s, reward_break=True):
+def take_break(s, reward_break=True, acc_min_change=.2, time_min_change=10, t=4, refractory_period=4, max_study_time=15):
     '''
     Intended for determination of breaks for reward and frustration break scenarios
     (expGroup == 2 or expGroup == 3)
     Parameters:
         s: session object
         reward_break: True if reward breaks, False if frustration breaks
-
+        t: int, consistency value
+        refractory_period: int, for super rule 2 (no break if less than refractory_period questions answered since last break)
+        max_study_time: int, for super rule 3 (take break if no break has been taken in last max_study_time minutes)
     returns (boolean, string)
         boolean: represents whether or not to trigger a break
         string: message representing reasoning for break
@@ -38,8 +40,8 @@ def take_break(s, reward_break=True):
     break_trigger = False
     break_val = 0
     total_accuracy = s.calc_total_accuracy()
-    accuracy_change = s.calc_accuracy_change()  # -1 if decrease, 0 if no change, 1 if increased
-    time_change = calc_time_change(s)  # -1 if decrease, 0 if no change, 1 if increased
+    accuracy_change = calc_accuracy_change(s, min_change=acc_min_change)  # -1 if decrease, 0 if no change, 1 if increased
+    time_change = calc_time_change(s, min_change=time_min_change)  # -1 if decrease, 0 if no change, 1 if increased
 
     if accuracy_change > 0:  # accuracy increase
         if time_change <= 0:  # time faster or no change
@@ -64,7 +66,7 @@ def take_break(s, reward_break=True):
                 break_trigger = True
                 break_val = 5
             else:  # no change
-                (break_trigger, break_val) = check_consistency(s, reward_break)
+                (break_trigger, break_val) = check_consistency(s, reward_break, t)
         else:  # overall accuracy < 80%
             if time_change > 0:  # time slower
                 break_trigger = True
@@ -73,11 +75,62 @@ def take_break(s, reward_break=True):
                 break_trigger = True
                 break_val = 9
             else:  # no change
-                (break_trigger, break_val) = check_consistency(s, reward_break)
+                (break_trigger, break_val) = check_consistency(s, reward_break, t)
+
+    # break_trigger depends on whether or not reward_break:
+    if reward_break:  # reward
+        if break_val > 4:  # i.e. a break that only occurs on frustration
+            break_trigger = False
+    else:  # frustration
+        if break_val <= 4:  # i.e. a break that only occurs on reward
+            break_trigger = False
+
+    # super rule #2: no break if < 4 questions answered since last break
+    if break_trigger:  # if break will be taken...
+        break_trigger = super_rule2(s, refractory_period=refractory_period)
+
+    # super rule #3: take break if no break has been taken in last 15 minutes
+    if not break_trigger:
+        break_trigger = super_rule3(s, max_study_time=max_study_time)
 
     # finally, insert this break into session object
+    # note, b_type could be of type that expects a break_trigger, except break_trigger might
+    # be inconsistent because it depends on reward_break type!
     s.insert_break(b_type=break_val, triggered_break=break_trigger)
     return (break_trigger, map_break_message(break_val))
+
+
+def super_rule3(s, max_study_time=15):
+    '''
+    returns True if break must be taken because of super rule 3, False otherwise (i.e. break could be taken, maybe)
+    '''
+    max_time_ms = max_study_time*60000  # convert to ms
+    current_time_since_start_ms = s.time_step()
+
+    # check if there has been break in last max_time_ms 
+    breaked_recently = False
+    for b in reversed(s.breaks):
+        if current_time_since_start_ms - b.time_since_start < max_time_ms:
+            if b.triggered_break:
+                breaked_recently = True
+        else:  # breaks too far in past to count
+            breaked_recently = False
+            break
+
+    return breaked_recently
+
+def super_rule2(s, refractory_period=4):
+    '''
+    returns True if break_trigger could be true, False if it fails this rule
+    '''
+    num_questions_since_last_break = 0
+    for b in reversed(s.breaks):
+        if b.triggered_break:
+            break
+        else:
+            num_questions_since_last_break += 1
+
+    return (num_questions_since_last_break >= refractory_period)
 
 
 def check_consistency(s, reward_break, t=4):
@@ -111,7 +164,7 @@ def check_consistency(s, reward_break, t=4):
             break_val = 3
         else:
             break_val = 8
-    elif in_a_row < t:b
+    elif in_a_row < t:
         break_trigger = False
         if reward_break:
             break_val = 4
@@ -121,8 +174,6 @@ def check_consistency(s, reward_break, t=4):
         print 'error in check_consistency: logic'
 
     return (break_trigger, break_val)
-
-
 
 
 def calc_time_change(s, min_change=10):
@@ -143,7 +194,6 @@ def calc_time_change(s, min_change=10):
         return -1
     else:
         return 0
-
 
 
 def calc_accuracy_change(s, min_change=.2):
