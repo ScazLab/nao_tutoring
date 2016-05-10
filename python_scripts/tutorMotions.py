623b47f8996090af39af424dad9cf0512b9b6b89
Arsalan Sufi <arsalansufi@gmail.com>
2016-05-09 22:01:48 -0400

    Fill out stretchBreak() function in tutorMotions.py

--- python_scripts/tutorMotions.py
+++ python_scripts/tutorMotions.py
@@ -992,4 +992,97 @@ class Gesture:
             print "Error when sitting down nao and making nao unstiff: "+str(e)
 
     def stretchBreak(self):
-        pass
+        self.posture.goToPosture("Sit", 1.0)
+        # I intentionally mispelled "lead" to make the speech clearer!
+        self.genSpeech("Follow my leed!")
+        time.sleep(2.5)
+
+        self.genSpeech("First spread your left arm out.")
+        self.motion.setAngles("RShoulderPitch", -1.0, 0.1)
+        self.motion.setAngles("RShoulderRoll", -1.2, 0.1)
+        self.motion.setAngles("RElbowRoll", 0.0, 0.1)
+        time.sleep(4.0)
+        self.genSpeech("Now spread your right arm out.")
+        self.motion.setAngles("LShoulderPitch", -1.0, 0.1)
+        self.motion.setAngles("LShoulderRoll", 1.2, 0.1)
+        self.motion.setAngles("LElbowRoll", 0.0, 0.1)
+        time.sleep(3.0)
+        self.genSpeech("Hold this position for a few seconds.")
+        time.sleep(7.0)
+
+        self.genSpeech("Raise both arms up.")
+        self.motion.setAngles("RShoulderRoll", 0.0, 0.1)
+        self.motion.setAngles("LShoulderRoll", 0.0, 0.1)
+        time.sleep(3.0)
+        self.genSpeech("Stretch your fingers out too!")
+        self.motion.openHand("RHand")
+        self.motion.openHand("LHand")
+        self.genSpeech("Count to 10 with me while we keep our arms up.")
+        time.sleep(4.0)
+        for i in xrange(10):
+            self.genSpeech(str(i + 1))
+            time.sleep(1)
+        self.motion.closeHand("RHand")
+        self.motion.closeHand("LHand")
+
+        self.genSpeech("Drop your arms down one at a time.")
+        time.sleep(3.0)
+        self.genSpeech("Start with your left arm.")
+        self.motion.setAngles("RShoulderPitch", 0.2, 0.1)
+        time.sleep(4.0)
+        self.genSpeech("Then your right arm.")
+        self.motion.setAngles("LShoulderPitch", 0.2, 0.1)
+        time.sleep(4.0)
+
+        self.genSpeech("Rotate your arms so that your palms are facing each other.")
+        time.sleep(3.0)
+        self.motion.setAngles("RElbowYaw", 1.5, 0.1)
+        self.motion.setAngles("LElbowYaw", -1.5, 0.1)
+        time.sleep(4.0)
+
+        self.genSpeech("Bend your left elbow.")
+        self.motion.setAngles("RElbowRoll", 1.4, 0.1)
+        time.sleep(4.0)
+        self.genSpeech("Then pull your left arm back.")
+        self.motion.setAngles("RShoulderRoll", -1.2, 0.1)
+        time.sleep(5.0)
+        self.genSpeech("Now bend your right elbow.")
+        self.motion.setAngles("LElbowRoll", -1.4, 0.1)
+        time.sleep(4.0)
+        self.genSpeech("Pull your right arm back just like your left.")
+        self.motion.setAngles("LShoulderRoll", 1.2, 0.1)
+        time.sleep(5.0)
+
+        self.genSpeech("Straighten your arms out.")
+        self.motion.setAngles("LElbowRoll", 0.0, 0.1)
+        self.motion.setAngles("RElbowRoll", 0.0, 0.1)
+        time.sleep(5.0)
+        self.genSpeech("Now drop your arms to your side.")
+        self.motion.setAngles("LShoulderPitch", 1.4, 0.1)
+        self.motion.setAngles("LShoulderRoll", 0.5, 0.1)
+        self.motion.setAngles("RShoulderPitch", 1.4, 0.1)
+        self.motion.setAngles("RShoulderRoll", -0.5, 0.1)
+        time.sleep(5.0)
+
+        self.genSpeech("Turn your head to the left.")
+        self.motion.setAngles("HeadYaw", -0.7, 0.1)
+        time.sleep(4.0)
+        self.genSpeech("Now to the right.")
+        self.motion.setAngles("HeadYaw", 0.7, 0.1)
+        time.sleep(4.0)
+        self.genSpeech("Bring it back to the center and look up.")
+        self.motion.setAngles("HeadYaw", 0.0, 0.1)
+        self.motion.setAngles("HeadPitch", -0.5, 0.1)
+        time.sleep(4.0)
+        self.genSpeech("Let's count to 10 one more time.")
+        time.sleep(4.0)
+        for i in xrange(10):
+            self.genSpeech(str(i + 1))
+            time.sleep(1)
+
+        self.genSpeech(
+            "Great job following along! I hope that was relaxing. Let's get back to our math "
+            "problems now. Click the button at the bottom of the tablet to return to the tutoring "
+            "session."
+        )
+        self.posture.goToPosture("Sit", 0.3)
