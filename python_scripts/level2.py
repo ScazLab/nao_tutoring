58f028ae2fad0d14da723893f2e3d286e21e2b62
annieechen <annie.chen@yale.edu>
2016-05-19 17:34:41 -0400

    level2 with mistake fields for both categories

--- python_scripts/level2.py
+++ python_scripts/level2.py
@@ -19,7 +19,15 @@ for problem in range(50):
 		answer = s1 + (a1 *b1) - (a2 * b2)
 		print '	"Answer" : %d,' % (answer)
 		print '	"Spoken Answer" : "The correct answer is %s.",' %(num2words(answer))
-		print '	"Most Common Mistake" : %d,' % ((s1 + a1) * b1 - (a2 * b2))
+		commonmistake1 =  ((s1 + a1) * b1 - (a2 * b2))
+		spokenexplanation1 = "Remember to do multiplication first! So multiply %s by %s and %s by %s" % (num2words(a1), num2words(b1), num2words(a2), num2words(b2))
+		print '		"Mistakes" : {'
+		print '		"Common Mistake 1" : %d,'% commonmistake1
+		print '		"Spoken Explanation 1" : " %s",' % (spokenexplanation1)
+		print '		"Written Explanation" : "Multiply %s by %s and %s by %s, and subtract the second product from the first. Then, add %s to that difference",' % (num2words(a1), num2words(b1), num2words(a2), num2words(b2), num2words(s1))
+		print ' 	"Written General Feedback" : "%d X %d = %d; %d X %d = %d; %d - %d = %d; %d + %d = %d"' %(a1,b1, a1*b1, a2, b2, a2*b2, a1*b1, a2*b2,a1*b1- a2*b2, s1, a1*b1- a2*b2, answer)
+		print '		},'
+
 	# want addition
 	else:
 		print '	"Question" : "What is %d + %d X %d + %d X %d?",' % (s1, a1, b1, a2, b2)
@@ -27,7 +35,14 @@ for problem in range(50):
 		answer = s1 + (a1 *b1) + (a2 * b2)
 		print '	"Answer" : %d,' % (answer)
 		print '	"Spoken Answer" : "The correct answer is %s.",' %(num2words(answer))
-		print '	"Most Common Mistake" : %d,' % ((s1 + a1) * b1 + (a2 * b2))
+		commonmistake1 =  ((s1 + a1) * b1 + (a2 * b2))
+		spokenexplanation1 = "Remember to do multiplication first! So multiply %s by %s and %s by %s" % (num2words(a1), num2words(b1), num2words(a2), num2words(b2))
+		print '		"Mistakes" : {'
+		print '		"Common Mistake 1" : %d,'% commonmistake1
+		print '		"Spoken Explanation 1" : " %s",' % (spokenexplanation1)
+		print '		"Written Explanation" : "Multiply %s by %s and %s by %s, and add the second product to the first. Then, add %s to that sum",' % (num2words(a1), num2words(b1), num2words(a2), num2words(b2), num2words(s1))
+		print ' 	"Written General Feedback" : "%d X %d = %d; %d X %d = %d; %d + %d = %d; %d + %d = %d"' %(a1,b1, a1*b1, a2, b2, a2*b2, a1*b1, a2*b2,a1*b1 + a2*b2, s1, a1*b1 + a2*b2, answer)
+		print '		},'
 	print '	"Difficulty Level" : 2,'
 	print '	"Problem Type" : "Multiplication", '
 	print '	"Max Time" : 60'
