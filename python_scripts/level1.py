b3e42aa437cbb31eaf49453b2bdd6f1a62bd9b13
annieechen <annie.chen@yale.edu>
2016-05-23 15:38:39 -0400

    fixing up minor errors in level1

--- python_scripts/level1.py
+++ python_scripts/level1.py
@@ -25,7 +25,7 @@ for number in range(20):
 			print '		"Common Mistake 1" : %d,' % ((third + number) * multiplier)
 			print '		"Spoken Explanation 1" : "I think you added %s and %s before you multiplied by %s.",' % (num2words(third), num2words(number), num2words(multiplier))
 			print '		"Written Explanation" : "Multiply %d X %d before adding %d to that product",' %(number, multiplier, third)
-			print '		"Written General Feedback" : "%d X %d = %d; Now we have %d + %d = %d"' %(number, multiplier, number*multiplier, third, number*multiplier, third + (number*multiplier))
+			print '		"Written General Feedback" : "%d X %d = %d; Now we have: %d + %d = %d"' %(number, multiplier, number*multiplier, third, number*multiplier, third + (number*multiplier))
 			print '	}'
 			print ' },'
 			print ""
@@ -33,13 +33,18 @@ for number in range(20):
 			print ' {'
 			print '	"QuestionID" : %d,' % counter
 			counter = counter + 1
+			#keep the multiple under
 			# counter already an odd number
 			if number % 2 == 0:
+				while third * number > 10:
+					third  = random.randint(1,9)
 				print '	"Question" : "What is (%d + %d) X %d?",' % (third, number, multiplier)
 				print '	"Spoken Question" : "What is (%s plus %s) times %s?", ' % (num2words(third), num2words(number), num2words(multiplier))
 				answer = ((third + number) * multiplier)
 				print '	"Answer" : %d,' % answer
 			else:
+				while number * multiplier > 10:
+					multiplier = random.randint(0,9)
 				print '	"Question" : "What is %d X (%d + %d)?",' % (third, number, multiplier)
 				print '	"Spoken Question" : "What is %s times (%s plus %s?)", ' % (num2words(third), num2words(number), num2words(multiplier))
 				answer =  (third * (number + multiplier))
@@ -53,14 +58,14 @@ for number in range(20):
 				print '		"Common Mistake 1" : %d,' % (third + number * multiplier)
 				print '		"Spoken Explanation 1" : "I think you multiplied %s and %s before you added %s.",' % (num2words(number), num2words(multiplier), num2words(third))
 				print '		"Written Explanation" : "Add %d + %d before multiplying %d with that sum",' %(third,number, multiplier)
-				print '		"Written General Feedback" : "%d + %d = %d; Now we have %d X %d = %d"' %(third, number,third + number, third + number, multiplier, ((third + number) * multiplier))
+				print '		"Written General Feedback" : "%d + %d = %d; Now we have: %d X %d = %d"' %(third, number,third + number, third + number, multiplier, ((third + number) * multiplier))
 				print '	}'
 			else:
 				print '	"Mistakes" : {'
 				print '		"Common Mistake 1" : %d,' % (third * number + multiplier)
 				print '		"Spoken Explanation 1" : "I think you multiplied %s and %s before you added %s.",' % (num2words(third), num2words(number), num2words(multiplier))
 				print '		"Written Explanation" : "Add %d + %d before multiplying %d with that sum",' %(multiplier,number, third)
-				print '		"Written General Feedback" : "%d + %d = %d; Now we have %d X %d = %d"' %(number, multiplier, multiplier + number, third, multiplier + number,  third * (number + multiplier))
+				print '		"Written General Feedback" : "%d + %d = %d; Now we have: %d X %d = %d"' %(number, multiplier, multiplier + number, third, multiplier + number,  third * (number + multiplier))
 				print '	}'
 			print ' },'
 			print ""
